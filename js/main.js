beb8fde009f1a9b0bc723c1b2859a0b62e883805
Aleksandr Kolmakov <65771833+aleksandr-kolmakov@users.noreply.github.com>
2020-10-23 22:17:33 +0300

    slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 $(function(){
+    $('.slider__inner').slick({
+        
+        arrows: false,
+        asNavFor:'.rtumb', 
+    });
+
+    $('.rtumb').slick({
+        arrows: false,
+        dots: false,
+        slidesToShow: 6,
+        slidesToScroll: 6,
+        asNavFor: '.slider__inner',
+        focusOnSelect: true,
+    });
 
 });
+
+  
+  
+  
+  
+  
